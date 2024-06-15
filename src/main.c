8b370c1ba4e64f843830b612098c084ea518d556
Marleen Vos <marleen.vos@protonmail.com>
2024-06-15 21:10:30 +0200

    add done and todo lists

--- src/main.c
+++ src/main.c
@@ -9,6 +9,29 @@
 
 #define SLEEP_TIME_MS 500
 
+/**
+ * DONE:
+ *    leds
+ *    button
+ * TODO:
+ *    usarts
+ *    512-Mbit octal-SPI Flash memory, 64-Mbit octal-SPI PSRAM, 256-Kbit I2C EEPROM
+ *    usb_fs
+ *    wifi
+ *    ble
+ *    microphones
+ *    2 digital microphones
+ *    Relative humidity and temperature sensor
+ *    3-axis magnetometer
+ *    3D accelerometer and 3D gyroscope
+ *    Pressure sensor, 260-1260 hPa absolute digital output barometer
+ *    Time-of-flight and gesture-detection sensor
+ *    Ambient-light sensor
+ *    DAC
+ *    ADC
+ *    pwm die temperature sensor
+ */
+
 void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
     printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
